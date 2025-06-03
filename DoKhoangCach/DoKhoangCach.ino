1dd884223a26960add43e9a6151521b321e6f2c9
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-06-03 15:39:32 +0700

    Hoàn thiện đo khoảng cách

--- DoKhoangCach/DoKhoangCach.ino
+++ DoKhoangCach/DoKhoangCach.ino
@@ -1,32 +1,47 @@
+#include <LiquidCrystal_I2C.h>
+
 const int trig = 8;  // chân trig của HC-SR04
 const int echo = 7;  // chân echo của HC-SR04
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
-  Serial.begin(9600);
-  pinMode(trig, OUTPUT);  // chân trig sẽ phát tín hiệu
-  pinMode(echo, INPUT);   // chân echo sẽ nhận tín hiệu
+    Serial.begin(9600);
+    pinMode(trig, OUTPUT);  // chân trig sẽ phát tín hiệu
+    pinMode(echo, INPUT);   // chân echo sẽ nhận tín hiệu
+
+	lcd.init();
+	lcd.backlight();
 }
 
 void loop() {
-  unsigned long duration;  // biến đo thời gian
-  float distance;            // biến lưu khoảng cách
+    unsigned long duration;  // biến đo thời gian
+    float distance;          // biến lưu khoảng cách
 
-  /* Phát xung từ chân trig */
-  digitalWrite(trig, 0);  // tắt chân trig
-  delayMicroseconds(2);
-  digitalWrite(trig, 1);  // phát xung từ chân trig
-  delayMicroseconds(5);   // xung có độ dài 5 microSeconds
-  digitalWrite(trig, 0);  // tắt chân trig
+    /* Phát xung từ chân trig */
+    digitalWrite(trig, 0);  // tắt chân trig
+    delayMicroseconds(2);
+    digitalWrite(trig, 1);  // phát xung từ chân trig
+    delayMicroseconds(5);   // xung có độ dài 5 microSeconds
+    digitalWrite(trig, 0);  // tắt chân trig
 
-  /* Tính toán thời gian */
-  // Đo độ rộng xung HIGH ở chân echo.
-  duration = pulseIn(echo, HIGH);
-  // Tính khoảng cách đến vật.
-  // Khoảng cách bằng (thời gian * tốc độ âm thanh (343 cm/us)) chia 2 (vì tín hiệu đi và về)
-  distance = (duration * 0.0343) / 2;
+    /* Tính toán thời gian */
+    // Đo độ rộng xung HIGH ở chân echo.
+    duration = pulseIn(echo, HIGH);
+    // Tính khoảng cách đến vật.
+    // Khoảng cách bằng (thời gian * tốc độ âm thanh (343 cm/us)) chia 2 (vì tín hiệu đi và về)
+    distance = (duration * 0.0343) / 2;
 
-  /* In kết quả ra Serial Monitor */
-  Serial.print(distance);
-  Serial.println("cm");
-  delay(200);
+    // In kết quả ra Serial Monitor
+    Serial.print(distance);
+    Serial.println("cm");
+
+	// In kết quả ra LCD
+	lcd.setCursor(0, 0);
+	lcd.print("Khoang cach: ");
+	lcd.setCursor(0, 1);
+	lcd.print(distance, 2);
+    lcd.print(" cm");
+
+    delay(500);
 }
