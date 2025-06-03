a1774c58eed50f0476ad5f4a30bbd2fb09ff0a29
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-06-03 16:13:36 +0700

    Hoàn thiện load cell

--- DoLuc/DoLuc.ino
+++ DoLuc/DoLuc.ino
@@ -2,63 +2,51 @@
 #include <LiquidCrystal_I2C.h>
 #include <HX711.h>
 
-// HX711 circuit wiring
-const int LOADCELL_DOUT_PIN = 2; // Chân DOUT của cảm biến loadcell kết nối với chân 2 Arduino
-const int LOADCELL_SCK_PIN = 3;	 // Chân SCK của cảm biến loadcell kết nối với chân 3 Arduino
+const int DOUT_PIN = 7;
+const int SCK_PIN = 8;
 
-HX711 scale; // Tạo đối tượng scale để đọc giá trị từ loadcell
+HX711 scale;
+float hieuChinh = 0;
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Khởi tạo màn hình LCD 16x2 với địa chỉ I2C 0x27
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void setup()
-{
-	lcd.init();										  // Khởi tạo màn hình LCD
-	lcd.backlight();								  // Bật đèn nền LCD
-	scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN); // Khởi tạo cảm biến loadcell
+void setup() {
+    lcd.init();
+    lcd.backlight();
+
+	// Khởi tạo HX711
+    scale.begin(DOUT_PIN, SCK_PIN);
+    // Set tỷ lệ ban đầu
+    scale.set_scale();
+    // Tare
+    scale.tare();
 }
 
-void loop()
-{
-	lcd.clear(); // Xóa màn hình LCD
+void loop() {
+    lcd.clear();
 
-	if (scale.is_ready())
-	{					   // Kiểm tra xem cảm biến có sẵn sàng không
-		scale.set_scale(); // Thiết lập tỷ lệ cân
-		lcd.setCursor(0, 0);
-		lcd.print("Dang tare..."); // Hiển thị đang thực hiện tare (cân zero)
-		delay(2000);			   // Đợi 5 giây
+    if (scale.is_ready()) {
+        lcd.print("Dang can...");
 
-		scale.tare(); // Thực hiện tare (cân zero)
-		lcd.clear();
-		lcd.setCursor(0, 0);
-		lcd.print("Tare xong"); // Hiển thị đã tare xong
-		lcd.setCursor(0, 1);
-		lcd.print("Dang can"); // Hiển thị đang cân
-		delay(3000);		   // Đợi 5 giây
+		scale.set_scale(hieuChinh);
+        long reading = scale.get_units(10);
 
-		long reading = scale.get_units(10); // Đọc giá trị cân (lấy trung bình 10 lần đọc)
-		lcd.clear();
-		lcd.setCursor(0, 0);
-		lcd.print("Can duoc:"); // Hiển thị kết quả cân
-		lcd.setCursor(0, 1);
-		lcd.print(reading); // In giá trị cân được
-	}
-	else
-	{
-		lcd.setCursor(0, 0);
-		lcd.print("Khong tim thayHX711"); // Hiển thị lỗi nếu không tìm thấy cảm biến
-		lcd.setCursor(0, 1);
-		lcd.print("HX711");
-	}
+        lcd.setCursor(0, 0);
+        lcd.print("Can duoc:");
+        lcd.setCursor(0, 1);
+        lcd.print(reading);
+    } else {
+        lcd.setCursor(0, 0);
+        lcd.print("HX711");
+        lcd.setCursor(0, 1);
+        lcd.print("Khong san sang");
+    }
 
-	delay(3000); // Đợi 3 giây trước khi lặp lại
+    delay(3000);
 }
 
-// HX711
+// LoadCell - HX711
 // Đỏ E+
 // Đen E-
 // Trắng A-
-// Đỏ A+
-
-// SCK D3
-// DOUT D2
+// Xanh A+
