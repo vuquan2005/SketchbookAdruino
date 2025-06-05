1918a7e46ab13d2bc0af6a0b1101649cbf5a5cf6
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-06-05 22:34:04 +0700

    Update Encoder.ino

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -3,83 +3,70 @@
 #define PIN_A 2  // Xanh
 #define PIN_B 3  // Trắng
 
-volatile long position = 0;
-int lastA = LOW;
+volatile long viTri = 0;
+int lastA = 0;
 
 int motVongQuay = 800;
 float vongQuay = 0;
 
 float vanToc = 0;
-long lastPosition = 0;
+long viTriCu = 0;
 const int delayTime = 500;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-    // Cấu hình chân
     pinMode(PIN_A, INPUT_PULLUP);
     pinMode(PIN_B, INPUT_PULLUP);
 
     attachInterrupt(digitalPinToInterrupt(PIN_A), readEncoder, CHANGE);
 
-    Serial.begin(9600);
-
+    // Serial.begin(9600);
     lcd.init();
     lcd.backlight();
-
-    lcd.setCursor(0, 0);
-    lcd.print("So vong: ");
-	lcd.setCursor(0, 1);
-	lcd.print("Van toc: ");
 }
 
 void loop() {
-    vongQuay = position / (float)motVongQuay;
+    vongQuay = viTri / (float)motVongQuay;
 
-    float vongQuayThayDoi = (position - lastPosition) / (float)motVongQuay;
+    float vongQuayThayDoi = (viTri - viTriCu) / (float)motVongQuay;
     vanToc = (vongQuayThayDoi / (delayTime / 60000.0));
 
 
-    Serial.print("So vong quay: ");
-    Serial.println(vongQuay);
+    // Serial.print("So vong quay: ");
+    // Serial.println(vongQuay);
+    // Serial.print("Van toc: ");
+    // Serial.print(vanToc);
+    // Serial.println(" v/p");
+    // Serial.print("Position: ");
+    // Serial.println(position);
 
-    Serial.print("Van toc: ");
-    Serial.print(vanToc);
-    Serial.println(" v/p");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("So vong: ");
+    lcd.print(vongQuay);
+    lcd.setCursor(0, 1);
+    lcd.print("Van toc: ");
+    lcd.print(vanToc, 2);
 
-    Serial.print("Position: ");
-    Serial.println(position);
-
-	lcd.setCursor(9, 0);
-	lcd.print(vongQuay);
-	lcd.setCursor(9, 1);
-	lcd.print(vanToc, 2);
-
-    lastPosition = position;
+    viTriCu = viTri;
     delay(delayTime);
 }
 
-// Hàm xử lý ngắt
 void readEncoder() {
     int currentA = digitalRead(PIN_A);
     int currentB = digitalRead(PIN_B);
 
-    // Xác định hướng quay
-    if (currentA != lastA) {     // Nếu A thay đổi
-        if (currentA == HIGH) {  // Cạnh lên của Channel A
-            if (currentB == LOW) {
-                position++;  // Quay thuận
-            } else {
-                position--;  // Quay ngược
-            }
-        } else {  // Cạnh xuống của Channel A
-            if (currentB == HIGH) {
-                position++;  // Quay thuận
-            } else {
-                position--;  // Quay ngược
-            }
+    // Nếu A thay đổi
+    if (currentA != lastA) {
+        if (currentA == 1 && currentB == 0 || currentA == 0 && currentB == 1) {
+            // A lên, B xuống hoặc A xuống, B lên => thuận
+            viTri++;
+        } else {
+            // A lên, B lên hoặc A xuống, B xuống => ngược
+            viTri--;
         }
     }
 
-    lastA = currentA;  // Cập nhật trạng thái trước đó
+    lastA = currentA;
 }
